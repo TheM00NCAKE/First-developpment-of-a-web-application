f5a2f89b0e37f2b8ceaa2fd7910f51b4466b2c10
Sarah <lbk.sarah06@gmail.com>
2025-05-21 13:24:05 +0000

    Jsp

---
+++ Model.py
@@ -0,0 +1,117 @@
+import sqlite3
+
+
+def connexion():
+    return sqlite3.connect("Indicateur_des_services.db")
+
+def create_table_Indicateur():
+    try:
+        with connexion() as conn:
+            cursor = conn.cursor()
+            sql = '''CREATE TABLE IF NOT EXISTS Indicateur(
+                        code_indicateur TEXT PRIMARY KEY,
+                        nom_indicateur TEXT,
+                        unite TEXT
+                        );'''
+           
+            cursor.execute(sql)
+            conn.commit()
+    except Exception as e:
+        print(f"Une erreur a été repéré pendant la création de la table: {e}")
+
+
+def create_table_Commune():
+    try:
+        with connexion() as conn:
+            cursor = conn.cursor()
+            sql = '''CREATE TABLE IF NOT EXISTS Commune(
+                        code_commune INTEGER PRIMARY KEY,
+                        nom_commune TEXT
+                        );'''
+           
+            cursor.execute(sql)
+            conn.commit()
+    except Exception as e:
+        print(f"Une erreur a été repéré pendant la création de la table: {e}")
+
+
+def create_table_Collectivite():
+    try:
+        with connexion() as conn:
+            cursor = conn.cursor()
+            sql = '''CREATE TABLE IF NOT EXISTS Collectivite(
+                        numero_collectivite INTEGER PRIMARY KEY,
+                        type_collectivite TEXT
+                        );'''
+           
+            cursor.execute(sql)
+            conn.commit()
+    except Exception as e:
+        print(f"Une erreur a été repéré pendant la création de la table: {e}")
+
+def create_table_Descriptif():
+    try:
+        with connexion() as conn:
+            cursor = conn.cursor()
+            sql = '''CREATE TABLE IF NOT EXISTS Descriptif(
+                        code_indicateur TEXT,
+                        code_service INTEGER,
+                        nom_service TEXT,
+                        numero_siren INTEGER,
+                        mode_gestion TEXT,
+                        code_commune INTEGER,
+                        numero_collectivite INTEGER,
+                        FOREIGN KEY (code_commune) REFERENCES Commune(code_commune),
+                        FOREIGN KEY (numero_collectivite) REFERENCES Collectivite(numero_collectivite),
+                        FOREIGN KEY (code_indicateur) REFERENCES Indicateur(code_indicateur),
+                        PRIMARY KEY (code_indicateur, code_service)
+
+                        );'''
+           
+            cursor.execute(sql)
+            conn.commit()
+    except Exception as e:
+        print(f"Une erreur a été repéré pendant la création de la table: {e}")
+
+
+
+def select_table(table):
+    try:
+        with connexion() as conn:
+            cursor = conn.cursor()
+            sql = f"SELECT * FROM {table};"
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            return rows
+    except Exception as e:
+        print(f"Erreur lors de la sélection dans la table: {e}")
+        return None
+
+def update_row(table, column, new_value, condition_column, condition_value):
+    try:
+        with connexion() as conn:
+            cursor = conn.cursor()
+            sql = f"UPDATE {table} SET {column} = ? WHERE {condition_column} = ?;"
+            cursor.execute(sql, (new_value, condition_value))
+            conn.commit()
+    except Exception as e:
+        print(f"Erreur lors de la mise à jour dans : {e}")
+
+
+def delete_row(table, column, value):
+    try:
+        with connexion() as conn:
+            cursor = conn.cursor()
+            sql = f"DELETE FROM {table} WHERE {column} = ?;"
+            cursor.execute(sql, (value,))
+            conn.commit()
+    except Exception as e:
+        print(f"Erreur lors de la suppression dans '{table}': {e}")
+
+
+create_table_Collectivite()
+create_table_Commune()
+create_table_Indicateur()
+create_table_Descriptif()
+
+
--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def accueil():
-    return render_template('index.html')
+@app.route("/")
+def index():    
+    return render_template("index.html")
 
-@app.route('/apropos')
+@app.route('/a_propos')
 def a_propos():
     return render_template('a_propos.html')
 
-@app.route('/indice')
-def indice():
-    return render_template('indice.html')
+@app.route('/Documentation')
+def Documentation():
+    return render_template('Documentation.html')
 
+@app.route('/Donnnées_indicateurs')
+def Données_indicateurs():
+    return render_template('Données_indicateurs.html')
 
 
 if __name__ == '__main__':
